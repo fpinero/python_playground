1e0901c101387a147d7b0d376bf58d6034602c46
Fernando P <grpeto@gmail.com>
2023-09-02 19:30:35 +0200

    added file that moves files from Downloads folter  into subfoldrs depending the type og the file. This is more fun with files

---
+++ files2/organize.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+# Path of the Download folder
+desktop_path = os.path.expanduser("~/Downloads")
+
+# Dictionary containing the folder names and their corresponding file extensions
+folders = {
+    "Images": [".jpeg", ".jpg", ".png", ".gif"],
+    "Documents": [".doc", ".docx", ".pdf", ".txt"],
+    "Archives": [".zip", ".rar"]
+}
+
+# Create the subfolders if they don't exist
+for folder_name in folders:
+    folder_path = os.path.join(desktop_path, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+# Move files to the corresponding subfolder
+for file_name in os.listdir(desktop_path):
+    original_file_path = os.path.join(desktop_path, file_name)
+    if os.path.isfile(original_file_path):
+        for folder_name, extensions in folders.items():
+            for extension in extensions:
+                if file_name.endswith(extension):
+                    destination_folder = os.path.join(desktop_path, folder_name)
+                    print("moving", original_file_path, 'to', destination_folder)
+                    shutil.move(original_file_path, destination_folder)
